94fc62b0be42e46625f12754498aa433f6c1f5cc
Lucas Nguyen <lhvnguyen@ucdavis.edu>
2024-05-01 20:14:47 -0700

    Tetris_done

--- tetris/tscript.js
+++ tetris/tscript.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 const canvasNext = document.getElementById('next');
 const ctxNext = canvasNext.getContext('2d');
 
+// Keep track of important values
 let accountValues = {
     score: 0,
     level: 0,
@@ -27,6 +28,7 @@ let account = new Proxy(accountValues, {
 let requestId = null;
 let time = null;
 
+// What happens to a shape when a key is pressed
 const moves = {
     [KEY.LEFT]: (p) => ({ ...p, x: p.x - 1 }),
     [KEY.RIGHT]: (p) => ({ ...p, x: p.x + 1 }),
@@ -53,6 +55,7 @@ function addEventListener() {
     document.addEventListener('keydown', handleKeyPress);
 }
 
+// Determine what happens when a certain key is pressed
 function handleKeyPress(event) {
     if (event.keyCode === KEY.P) {
         pause();
@@ -66,13 +69,6 @@ function handleKeyPress(event) {
         let p = moves[event.keyCode](board.piece);
         if (event.keyCode === KEY.SPACE) {
             // Hard drop
-            if (document.querySelector('#pause-btn').style.display === 'block') {
-                dropSound.play();
-            }
-            else{
-                return;
-            }
-            
             while (board.valid(p)) {
                 account.score += POINTS.HARD_DROP;
                 board.piece.move(p);
@@ -81,9 +77,7 @@ function handleKeyPress(event) {
             board.piece.hardDrop();
         } 
         else if (board.valid(p)) {
-            if (document.querySelector('#pause-btn').style.display === 'block') {
-                movesSound.play();
-            }
+            
             board.piece.move(p);
             if (event.keyCode === KEY.DOWN && 
                 document.querySelector('#pause-btn').style.display === 'block') {
@@ -93,6 +87,7 @@ function handleKeyPress(event) {
     }
 }
 
+// Wipe to start a new game
 function resetGame() {
     account.score = 0;
     account.lines = 0;
@@ -101,6 +96,7 @@ function resetGame() {
     time = { start: performance.now(), elapsed: 0, level: LEVEL[account.level] };
 }
 
+// Start the game when pressing the play button
 function play() {
     addEventListener();
     if (document.querySelector('#play-btn').style.display == '') {
@@ -115,11 +111,13 @@ function play() {
     animate();
     document.querySelector('#play-btn').style.display = 'none';
     document.querySelector('#pause-btn').style.display = 'block';
-    backgroundSound.play();
+    // backgroundSound.play();
 }
 
+// Animate falling pieces
 function animate(now = 0) {
     time.elapsed = now - time.start;
+
     if (time.elapsed > time.level) {
         time.start = now;
         if (!board.drop()) {
@@ -135,6 +133,7 @@ function animate(now = 0) {
     requestId = requestAnimationFrame(animate);
 }
 
+// Pieces stack too high -> Game Over
 function gameOver() {
     cancelAnimationFrame(requestId);
 
@@ -144,20 +143,19 @@ function gameOver() {
     ctx.fillStyle = 'red';
     ctx.fillText('GAME OVER', 1.8, 4);
     
-    sound.pause();
-    finishSound.play();
+    
     checkHighScore(account.score);
 
     document.querySelector('#pause-btn').style.display = 'none';
     document.querySelector('#play-btn').style.display = '';
 }
 
+// Pause the game
 function pause() {
     if (!requestId) {
         document.querySelector('#play-btn').style.display = 'none';
         document.querySelector('#pause-btn').style.display = 'block';
         animate();
-        backgroundSound.play();
         return;
     }
 
@@ -171,9 +169,9 @@ function pause() {
     ctx.fillText('PAUSED', 3, 4);
     document.querySelector('#play-btn').style.display = 'block';
     document.querySelector('#pause-btn').style.display = 'none';
-    sound.pause();
 }
 
+// Display the high scores
 function showHighScores() {
     const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
     const highScoreList = document.getElementById('highScores');
@@ -183,6 +181,7 @@ function showHighScores() {
     .join('');
 }
 
+// Check locally stored high scores
 function checkHighScore(score) {
     const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
     const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
@@ -195,6 +194,7 @@ function checkHighScore(score) {
     }
 }
 
+// Save a high score to the local tiny database
 function saveHighScore(score, highScores) {
     highScores.push(score);
     highScores.sort((a, b) => b.score - a.score);
